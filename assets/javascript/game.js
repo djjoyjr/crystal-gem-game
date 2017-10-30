68c8c2b670f051a2ecf320fe034bf68911fa1572
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2017-10-30 16:12:23 -0400

    Final Version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,31 +3,50 @@ $(document).ready(function() {
 var counter = 0;
 var wins = 0;
 var losses = 0;
-var crystalRandomValues = new Array(4);
-var targetNumber = Math.floor(Math.random() * 120) + 19;
-$("#number-to-guess").text(targetNumber);
-for (var i = 0; i < crystalRandomValues.length; i++) {
-  crystalRandomValues[i] = Math.floor(Math.random() * 12)+1;
-}
-for (var i = 0; i < crystalRandomValues.length; i++) {
+var imgSrc = ['assets/images/garnet.png', 'assets/images/amethyst.png', 'assets/images/pearl.png', 'assets/images/ruby.png'];
+var gifArray = ['https://media.giphy.com/media/VkPaYWleJugmc/giphy.gif', 'https://media.giphy.com/media/ETicwJxFobLLq/giphy.gif', 'https://media.giphy.com/media/qU64U6iHxnPfq/giphy.gif', 'https://media.giphy.com/media/8rJ1wPuzog1P2/giphy.gif'];
 
+targetNumber = Math.floor(Math.random() * 120) + 19;
+$("#number-to-guess").text(targetNumber);
+
+function crystalRandomValues() {
+  return Math.floor(Math.random()*12)+1;
+};
+
+for (var i = 0; i < imgSrc.length; i++) {
   var gemPic = $("<img>");
   gemPic.addClass("gem-pic");
-  gemPic.attr("src", "./assets/images/gem" + (i+1) + ".png");
-  gemPic.attr("data-crystalvalue", crystalRandomValues[i]);
+  gemPic.attr("src", imgSrc[i]);
+  gemPic.attr("data-crystalvalue", crystalRandomValues());
   $("#gemDiv").append(gemPic);
 }
 
+function reset() {
+  $('#number-to-guess').html(" ");
+  targetNumber = Math.floor(Math.random() * 120) + 19;
+  $("#number-to-guess").text(targetNumber);
+  counter=0;
+  var gemDivChildren = $("#gemDiv").children();
+  for (var i = 0; i < gemDivChildren.length; i++){
+  	$(gemDivChildren[i]).attr("data-crystalvalue", crystalRandomValues());
+  }
+}
+
 $(".gem-pic").on("click", function() {
+  $("#win-gif").html("");
   var crystalValue = ($(this).attr("data-crystalvalue"));
   crystalValue = parseInt(crystalValue);
   counter += crystalValue;
-    if (counter === targetNumber) {
-    alert("You win!");
+    if (counter == targetNumber) {
+    var j = Math.floor(Math.random()*4);
+    var winGif = $("<img>");
+    winGif.attr("src", gifArray[j]);
+    $("#win-gif").text("Congratulations, you win!  Click on any Crystal Gem to start a new game or you can just enjoy this gif for a while.")
+    $("#win-gif").append(winGif);
     wins ++;
     reset();
   }
-  else if (counter >= targetNumber) {
+  else if (counter > targetNumber) {
     alert("You lose!!");
     losses ++;
     reset();
@@ -36,16 +55,4 @@ $(".gem-pic").on("click", function() {
   $("#wins").html("<span>Wins:  </span>" + wins);
   $("#losses").html("<span>Losses:  </span>" + losses);
 });
-function reset() {
-    counter = 0;
-    targetNumber = Math.floor(Math.random() * 120) + 19;
-    $("#number-to-guess").text(targetNumber);
-    for (var i = 0; i < crystalRandomValues.length; i++) {
-      crystalRandomValues[i] = Math.floor(Math.random() * 12)+1;
-    }
-  }
-
-
-
-
 });
